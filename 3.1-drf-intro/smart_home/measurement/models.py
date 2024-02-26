a3b3345ea0d54617ac4b326441e93ae44fd43644
PrekinII <prekinivan@gmail.com>
2024-02-26 17:21:57 +0500

    DateTimeField

--- 3.1-drf-intro/smart_home/measurement/models.py
+++ 3.1-drf-intro/smart_home/measurement/models.py
@@ -9,5 +9,5 @@ class Sensor(models.Model):
 class Measurement(models.Model):
     sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE, related_name='measurements', null=True)
     temperature = models.DecimalField(max_digits=5, decimal_places=2)
-    created_at = models.DateField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     image = models.ImageField(null=True, blank=True, verbose_name='image')
