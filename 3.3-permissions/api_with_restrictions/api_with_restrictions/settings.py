0c97d44c8e057da50a47d97f9d9877e6ef5bccf6
PrekinII <prekinivan@gmail.com>
2024-03-16 18:22:08 +0500

    settings

--- 3.3-permissions/api_with_restrictions/api_with_restrictions/settings.py
+++ 3.3-permissions/api_with_restrictions/api_with_restrictions/settings.py
@@ -76,6 +76,7 @@ TEMPLATES = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_THROTTLE_CLASSES': [
         'rest_framework.throttling.AnonRateThrottle',
